0d3b2df1ccfd1310fe2ab4d7f38493c4916ee347
Dave Fisher <fisherds@rose-hulman.edu>
2014-09-27 00:39:32 -0400

    Remove model objects from start
    Also update app.yaml

--- models.py
+++ models.py
@@ -1,24 +1,3 @@
 from google.appengine.ext import ndb
 
-
-class Player(ndb.Model):
-  """ Information for a player. """
-  display_name = ndb.StringProperty()
-
-  def get_name(self):
-    """Returns the best name available for a Player."""
-    if self.display_name:
-      return self.display_name
-    return self.key.string_id() # email address
-
-
-class Game(ndb.Model):
-  """ Dice with Friends game. """
-  creator_key = ndb.KeyProperty(kind=Player)
-  invitee_key = ndb.KeyProperty(kind=Player)
-  creator_scores = ndb.IntegerProperty(repeated=True)
-  invitee_scores = ndb.IntegerProperty(repeated=True)
-  last_touch_date_time = ndb.DateTimeProperty(auto_now=True)
-  is_complete = ndb.BooleanProperty(default=False)
-
-  
+# TODO: Implement
