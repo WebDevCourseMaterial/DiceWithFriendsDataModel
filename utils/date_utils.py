3670ec293341795073e5c31e7b5c725302f9106f
Dave Fisher <fisherds@rose-hulman.edu>
2014-09-27 00:37:06 -0400

    Making the date_format VERY simple
    In the real version of this app the date formatter is broken at present

--- utils/date_utils.py
+++ utils/date_utils.py
@@ -1,10 +1,3 @@
 
 def date_format(value):
-  if value.year == value.year:
-    if value.month == value.month and value.day == value.day:
-      format_str = "Today"
-    elif value.year == value.year:
-      format_str = "%B %d"
-  else:
-    format_str = "%B %d, %Y"
-  return value.strftime(format_str).replace(" 0", " ")
+  return value.strftime("%B %d").replace(" 0", " ")
